2421d188efcde8a8c1582f931aba21b25feb0b9b
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-14 13:28:33 -0300

    :sparkles: (global-style) added global style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app.theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes no App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 5).theme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
